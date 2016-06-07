3a4467ce5c6b00390c54646469b27fddabeb8695
hadley <h.wickham@gmail.com>
2016-06-07 08:37:01 -0500

    Delete old loading code

--- load.r
+++
@@ -1,7 +0,0 @@
-library(ggplot)
-source.with.err <- function(path) {
-	tryCatch(source(path), error = function(x) print(path))
-}
-lapply(dir("~/Documents/ggplot/releases/ggplot-0.4.1/R/", full.name=T), source.with.err)
-
-
