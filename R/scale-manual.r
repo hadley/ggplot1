15621a0df4fd98831329adce1ffc190696343c34
hadley <h.wickham@gmail.com>
2016-06-07 16:48:19 -0500

    Document and export manual scale

--- R/scale-manual.r
+++ R/scale-manual.r
@@ -1,19 +1,14 @@
-# Scale: manual
-# Create a manual scale
-#
-# This scale function allows you complete control over the
-# scale.
-#
-# Supply labels and breaks to produce a legend.
-#
-# @keyword hplot
-# @arguments plot object to add scale to
-# @arguments variable to scale
-# @arguments name of the scale (used in the legend)
-# @arguments numeric vector of break points
-# @arguments character vector of break labels
-# @arguments grob function to use when drawing legend
-# @seealso \code{\link{ggfluctuation}} for a use
+#' Scale: manual
+#'
+#' This scale function allows you complete control over the
+#' scale.
+#'
+#' Supply labels and breaks to produce a legend.
+#'
+#' @inheritParams pscontinuous
+#' @param labels character vector of break labels
+#' @param grob grob function to use when drawing legend
+#' @export
 scmanual <- function(plot, variable="x", name="", breaks=NULL, labels=as.character(breaks), grob=function(x) grob_point(x, unique=FALSE)) {
 	add_scale(plot,
 	  scale_manual(variable=variable, name=name, breaks=breaks, labels=labels, grob=grob)
